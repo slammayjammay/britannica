bc4b59cb84bae9751b8d4eba2ba1d5f4cd147fbb
Scott McKenzie <slammayjammay@gmail.com>
2018-09-05 15:31:45 -0700

    Build

--- src/create-app.js
+++ src/create-app.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './js/components/App.vue';
 import createRouter from './create-router';
 
-const IS_GH_PAGES = false;
+const IS_GH_PAGES = true;
 
 export default () => {
 	const router = createRouter();
